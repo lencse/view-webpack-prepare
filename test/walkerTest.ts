c0c04b760dca0ed00969099037882d6f34f57a49
Levente Löki <leventeloki@gmail.com>
2019-01-28 22:40:21 +0100

    work

--- test/walkerTest.ts
+++ test/walkerTest.ts
@@ -1,4 +1,10 @@
+import { tmpdir } from 'os'
+import { join } from 'path'
+import { sync as mkdirSync} from 'mkdirp'
+
 describe('Walking source dir', () => {
     it('Walk empty dir', () => {
+        const dirname = join(tmpdir(), String(Date.now()))
+        mkdirSync(dirname)
     })
 })
