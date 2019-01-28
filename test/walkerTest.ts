b85d2e7ad0fa207c5376a2ba1ccd11cd760be31e
Levente Löki <leventeloki@gmail.com>
2019-01-28 22:53:20 +0100

    work

--- test/walkerTest.ts
+++ test/walkerTest.ts
@@ -4,10 +4,19 @@ import { sync as mkdirSync } from 'mkdirp'
 import Walker from '../src/walker/Walker'
 
 describe('Walking source dir', () => {
+
     it('Walk empty dir', async () => {
         const dirname = join(tmpdir(), String(Date.now()))
         mkdirSync(dirname)
         const walker = new Walker()
         expect(await walker.getFiles(dirname)).toEqual([])
+    })
+
+    it('Walk dir with one nonrelevant file', async () => {
+        const dirname = join(__dirname, 'one-nonrelevant-file')
+        const walker = new Walker()
+        expect(await walker.getFiles(dirname)).toEqual([
+            // join(dirname, '.gitkeep')
+        ])
     })
 })
