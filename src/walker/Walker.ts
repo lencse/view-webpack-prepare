b951b177444df546877f319a71c50ea82e8cde06
Levente Löki <leventeloki@gmail.com>
2019-01-28 22:46:32 +0100

    work

---
+++ src/walker/Walker.ts
@@ -0,0 +1,7 @@
+export default class Walker {
+
+    public async getFiles(): Promise<string[]> {
+        return []
+    }
+
+}
--- test/walkerTest.ts
+++ test/walkerTest.ts
@@ -1,12 +1,13 @@
 import { tmpdir } from 'os'
 import { join } from 'path'
 import { sync as mkdirSync } from 'mkdirp'
+import Walker from '../src/walker/Walker'
 
 describe('Walking source dir', () => {
-    it('Walk empty dir', () => {
+    it('Walk empty dir', async () => {
         const dirname = join(tmpdir(), String(Date.now()))
         mkdirSync(dirname)
-        return
-        const walker = {}
+        const walker = new Walker()
+        expect(await walker.getFiles()).toEqual([])
     })
 })
