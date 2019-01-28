f5aa832cc39970c042ca187f5e9bf7fb4b7087a5
Levente Löki <leventeloki@gmail.com>
2019-01-28 22:08:31 +0100

    work

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     testMatch: [
         '**/test/**/*Test.ts?(x)'
     ],
-    // coverageDirectory: 'logs/jest',
-    // collectCoverage: true,
+    coverageDirectory: 'logs/jest',
+    collectCoverage: true,
     // testResultsProcessor: 'jest-sonar-reporter'
 }
