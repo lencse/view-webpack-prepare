99d4637f54ade8448539f90356b1ac07a37d92d0
Levente Löki <leventeloki@gmail.com>
2019-01-29 17:59:02 +0100

    work

--- test/walkerTest.ts
+++ test/walkerTest.ts
@@ -32,7 +32,7 @@ describe('Walking source dir', () => {
         const dirname = join(process.cwd(), 'test', 'fixtures', 'templates-in-subdirs')
         const walker = new Walker()
         const files = walker.getFiles(dirname)
-        console.log(files)
+        console.log(files.length)
         // expect(files.length).toBe(3)
         // expect(walker.getFiles(dirname)).toEqual([
         //     join(dirname, 'index.html')
