aebb557d12b04b71666966446e9dec683a4785d2
Levente Löki <leventeloki@gmail.com>
2019-01-29 20:46:57 +0100

    work

---
+++ test/fileTest.ts
@@ -0,0 +1,12 @@
+import File from '../src/files/File'
+
+describe('Files', () => {
+
+    it('Transform path', () => {
+        const file = new File('/path/to/src/dir/subdir/', 'file')
+        expect(file.transformPath('/path/to/src', 'path/to/target')).toEqual(
+            new File('/path/to/src/dir/subdir/', 'file')
+        )
+    })
+
+})
