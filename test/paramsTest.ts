8e107d5a9e9fe9e8197794118a3631eded607b15
Levente Löki <leventeloki@gmail.com>
2019-01-29 18:32:53 +0100

    work

--- test/paramsTest.ts
+++
@@ -1,8 +0,0 @@
-import Params from '../src/Params'
-
-describe('Test params', () => {
-    it('Test srcDir', () => {
-        const params = new Params('srcDir')
-        expect(params.srcDir).toEqual('srcDir')
-    })
-})
