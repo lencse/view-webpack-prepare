e27f66a14e0f8dfbf9b8dbb3189b89630bce1d26
Levente Löki <leventeloki@gmail.com>
2019-01-29 22:55:07 +0100

    work

--- test/compilerTest.ts
+++ test/compilerTest.ts
@@ -3,14 +3,17 @@ import Compilers from '../src/compiler/Compilers'
 describe('Compilers', () => {
 
     const compilers = new Compilers()
-    compilers.add('html', (content: string, options: any) => content.toLowerCase())
+    compilers.add(
+        'html',
+        (content: string, options: any) => [content.toLowerCase(), options.foo].join('-')
+    )
 
     it('Default', () => {
         expect(compilers.get('txt')('TEST', {})).toEqual('TEST')
     })
 
     it('Get compiler', () => {
-        expect(compilers.get('html')('TEST', {})).toEqual('test')
+        expect(compilers.get('html')('TEST', {foo: 'bar'})).toEqual('test-bar')
     })
 
 })
