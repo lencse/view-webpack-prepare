748ec27cd2855747b3886e942eb2e42907db3ab8
Levente Löki <leventeloki@gmail.com>
2019-01-30 00:14:22 +0100

    work

--- lib/app/DirBurner.ts
+++ lib/app/DirBurner.ts
@@ -13,8 +13,10 @@ export default class DirBurner {
     public run(setup: Setup, params: any) {
         const { sourceDir, targetDir, targetExtension, compilers } = setup
         this.walker.getFiles(sourceDir).forEach((file) => {
-            const target = file.transformPath(sourceDir, targetDir).transformExtension(targetExtension)
-            this.burner.burn(target, compilers.get(file.extension)(content(file), params))
+            this.burner.burn(
+                file.transformPath(sourceDir, targetDir).transformExtension(targetExtension),
+                compilers.get(file.extension)(content(file), params)
+            )
         })
     }
 
