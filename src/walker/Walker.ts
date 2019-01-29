1b8eaaf3a63285e6f94d0efef6be983c5d246e8b
Levente Löki <leventeloki@gmail.com>
2019-01-29 15:23:19 +0100

    work

--- src/walker/Walker.ts
+++ src/walker/Walker.ts
@@ -3,6 +3,7 @@ import { walkSync } from 'walk'
 export default class Walker {
 
     public async getFiles(dir: string): Promise<string[]> {
+        console.log(dir)
         walkSync(dir, {
             listeners: {
                 names: (base, names, next) => {
