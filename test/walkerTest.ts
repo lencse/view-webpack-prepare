484c870a2293e0728cecd1e402382a27347422a0
Levente Löki <leventeloki@gmail.com>
2019-01-29 18:11:15 +0100

    work

--- test/walkerTest.ts
+++ test/walkerTest.ts
@@ -1,20 +1,20 @@
 import { tmpdir } from 'os'
 import { join } from 'path'
 import { sync as mkdirSync } from 'mkdirp'
-import Walker from '../src/walker/Walker'
+import NpmWalkWalker from '../src/walker/NpmWalkWalker'
 
 describe('Walking source dir', () => {
 
     it('Walk empty dir', () => {
         const dirname = join(tmpdir(), String(Date.now()))
         mkdirSync(dirname)
-        const walker = new Walker()
+        const walker = new NpmWalkWalker()
         expect(walker.getFiles(dirname)).toEqual([])
     })
 
     it('Walk dir with one nonrelevant file', async () => {
         const dirname = join(process.cwd(), 'test', 'fixtures', 'one-nonrelevant-file')
-        const walker = new Walker()
+        const walker = new NpmWalkWalker()
         expect(walker.getFiles(dirname)).toEqual([
             join(dirname, '.gitkeep')
         ])
@@ -22,7 +22,7 @@ describe('Walking source dir', () => {
 
     it('Walk dir with one html file', async () => {
         const dirname = join(process.cwd(), 'test', 'fixtures', 'one-html-file')
-        const walker = new Walker()
+        const walker = new NpmWalkWalker()
         expect(walker.getFiles(dirname)).toEqual([
             join(dirname, 'index.html')
         ])
@@ -30,7 +30,7 @@ describe('Walking source dir', () => {
 
     it('Walk dir with subdirs', async () => {
         const dirname = join(process.cwd(), 'test', 'fixtures', 'templates-in-subdirs')
-        const walker = new Walker()
+        const walker = new NpmWalkWalker()
         const files = walker.getFiles(dirname)
         expect(files.length).toBe(3)
         const test = ['index.html', 'subdir1/tpl.html', 'subdir2/tpl.html']
