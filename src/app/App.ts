1eb871585404d3d7723dfcf59d69d1f222d052ec
Levente Löki <leventeloki@gmail.com>
2019-01-29 22:27:25 +0100

    work

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,14 +1,21 @@
 import Walker from '../walker/Walker'
 import Setup from './Setup'
+import FileBurner from '../files/FileBurner'
+import content from '../files/content'
 
 export default class App {
 
     constructor(
-        private walker: Walker
+        private walker: Walker,
+        private burner: FileBurner
     ) {}
 
     public run(setup: Setup) {
-        this.walker.getFiles(setup.sourceDir)
+        const { sourceDir, targetDir, targetExtension, compilers } = setup
+        this.walker.getFiles(sourceDir).forEach((file) => {
+            const target = file.transformPath(sourceDir, targetDir).transformExtension(targetExtension)
+            this.burner.burn(target, compilers.get(file.extension)(content(file)))
+        })
     }
 
 }
--- test/appTest.ts
+++ test/appTest.ts
@@ -4,11 +4,13 @@ import App from '../src/app/App'
 import NpmWalkWalker from '../src/walker/NpmWalkWalker'
 import Setup from '../src/app/Setup'
 import Compilers from '../src/compiler/Compilers'
+import FileBurner from '../src/files/FileBurner'
+import MkdirpDirCreator from '../src/dirs/MkdirpDirCreator'
 
 describe('App', () => {
 
     it('Run app', () => {
-        const app = new App(new NpmWalkWalker())
+        const app = new App(new NpmWalkWalker(), new FileBurner(new MkdirpDirCreator()))
         const targetDir = join(tmpdir(), String(Date.now()))
         const compilers = new Compilers()
         const setup = new Setup(
